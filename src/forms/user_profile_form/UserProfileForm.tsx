4f0d7ad6bc1796dbc3d5186ea8bdc6c879f0c932
harshita-2003 <harshitag374@gmail.com>
2024-09-21 16:53:11 +0530

    removed loading import

--- src/forms/user_profile_form/UserProfileForm.tsx
+++ src/forms/user_profile_form/UserProfileForm.tsx
@@ -16,7 +16,6 @@ import LoadingButton from "@/components/LoadingButton";
 import { User } from "@/types";
 import { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import LoadingScreen from "@/components/LoadingScreen";
 
 const formSchema = z.object({
   email: z.string().optional(),
